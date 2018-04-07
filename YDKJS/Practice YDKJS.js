006607c777234b1c9946866c488991e04d9817a5
Angelina <angelinalblyth@hotmail.com>
2018-04-07 17:02:30 +0100

    Practice for YDKJS

---
+++ YDKJS/Practice YDKJS.js
@@ -0,0 +1,33 @@
+const taxRate = 0.08;
+const phonePrice = 99.99;
+const accessoryPrice = 9.99; 
+const spendingThreshold = 200;
+
+var bankBalance = 308.00;
+var amount = 0; 
+
+//functions
+
+function calculateTax (amount) {
+	return amount * taxRate;
+}
+
+//format
+
+function formatCost(amount) { 
+	return "£" + amount.toFixed(2);
+}
+
+//while loop 
+
+while (amount < bankBalance) {
+	amount = amount + phonePrice;
+
+	if(amount < spendingThreshold) {
+		amount = amount + accessoryPrice;
+	}
+}
+
+amount = amount + calculateTax(amount);
+
+console.log("Your balance is " + formatCost(amount))
