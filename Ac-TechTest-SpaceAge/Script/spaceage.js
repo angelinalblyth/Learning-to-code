a432adaef0ccf85b3060145244c9ffa96bc9ee6a
Angelina <angelinalblyth@hotmail.com>
2018-04-10 22:29:33 +0100

    added comments

--- Ac-TechTest-SpaceAge/Script/spaceage.js
+++ Ac-TechTest-SpaceAge/Script/spaceage.js
@@ -1,3 +1,5 @@
+
+
 var planets = [
 	{planet: 'earth', orbit:1},
 	{planet: 'mercury', orbit:0.2408467},
@@ -8,8 +10,12 @@ var planets = [
 	{planet: 'neptune', orbit:164.79132},
 	{planet: 'pluto', orbit: 248},
 	]
+
+//Ask users for age
 var age = prompt("How old are you on Earth?")
 
+
+//Calculate age on other planets
 for (i=0; i< planets.length; i++) {
 	var planetAge = age / planets[i].orbit;
 	console.log("On " + planets[i].planet + " you are " + planetAge);
