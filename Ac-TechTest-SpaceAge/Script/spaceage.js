854d46ef7ed0d5d279fe8fd4448e779d25f4a68b
Angelina <angelinalblyth@hotmail.com>
2018-04-11 23:00:34 +0100

    JS now works and displays correctly
    Missed out a planet. Uranus :smirk:

--- Ac-TechTest-SpaceAge/Script/spaceage.js
+++ Ac-TechTest-SpaceAge/Script/spaceage.js
@@ -1,19 +1,24 @@
 function calculate() {
   var age = document.getElementById("age").value;
-  
+
   var planets = [
-	{planet: 'earth', orbit:1},
-	{planet: 'mercury', orbit:0.2408467},
-	{planet: 'venus', orbit: 0.61519726},
-	{planet: 'mars', orbit: 1.888158},
-	{planet: 'jupitor', orbit:11.862615},
-	{planet: 'saturn', orbit:29.447498},
-	{planet: 'neptune', orbit:164.79132},
-	{planet: 'pluto', orbit: 248},
-	]
-  
-  for (i=0; i< planets.length; i++) {
-	  var planetAge = age / planets[i].orbit;
-    console.log("On " + planets[i].planet + " you are " + planetAge);
-}
+    { planet: "earth", orbit: 1 },
+    { planet: "mercury", orbit: 0.2408467 },
+    { planet: "venus", orbit: 0.61519726 },
+    { planet: "mars", orbit: 1.888158 },
+    { planet: "jupitor", orbit: 11.862615 },
+    { planet: "saturn", orbit: 29.447498 },
+    { planet: "uranus", orbit: 84.016846 },
+    { planet: "neptune", orbit: 164.79132 },
+    { planet: "pluto", orbit: 248 }
+  ];
+
+  for (i = 0; i < planets.length; i++) {
+    var planetAge = age / planets[i].orbit;
+
+    //This is changing the P tag that is under Mercury as it is the [1] p tag
+    //Change to [0] and it changes the first P tag 'Enter you age...'
+    document.getElementsByTagName("p")[i].innerHTML = planetAge;
+  }
+  document.getElementById("calculate").addEventListener("click", calculate);
 }
