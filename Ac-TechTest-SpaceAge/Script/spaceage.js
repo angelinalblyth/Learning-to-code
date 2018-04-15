52904cebbbfbdffb2f28080bff79a93c10632673
Angelina <angelinalblyth@hotmail.com>
2018-04-15 18:51:56 +0100

    Updated code from codepen

--- Ac-TechTest-SpaceAge/Script/spaceage.js
+++ Ac-TechTest-SpaceAge/Script/spaceage.js
@@ -6,19 +6,16 @@ function calculate() {
     { planet: "mercury", orbit: 0.2408467 },
     { planet: "venus", orbit: 0.61519726 },
     { planet: "mars", orbit: 1.888158 },
-    { planet: "jupitor", orbit: 11.862615 },
+    { planet: "jupiter", orbit: 11.862615 },
     { planet: "saturn", orbit: 29.447498 },
     { planet: "uranus", orbit: 84.016846 },
     { planet: "neptune", orbit: 164.79132 },
     { planet: "pluto", orbit: 248 }
   ];
 
-  for (i = 0; i < planets.length; i++) {
-    var planetAge = age / planets[i].orbit;
-
-    //This is changing the P tag that is under Mercury as it is the [1] p tag
-    //Change to [0] and it changes the first P tag 'Enter you age...'
-    document.getElementsByTagName("p")[i].innerHTML = planetAge + " years old";
+  for (i = 1; i <= planets.length; i++) {
+    var planetAge = age / planets[i - 1].orbit;
+    document.getElementsByTagName("p")[i].innerHTML = planetAge + "\n years old.";
   }
   document.getElementById("calculate").addEventListener("click", calculate);
 }
