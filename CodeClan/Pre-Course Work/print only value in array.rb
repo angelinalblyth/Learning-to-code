4e3791ea176fbd520243daaab805ef65726f73c9
Angelina <angelinalblyth@hotmail.com>
2018-04-24 22:23:21 +0100

    add challenge on only printing value

---
+++ CodeClan/Pre-Course Work/print only value in array.rb
@@ -0,0 +1,12 @@
+lunch_order = {
+  "Ryan" => "wonton soup",
+  "Eric" => "hamburger",
+  "Jimmy" => "sandwich",
+  "Sasha" => "salad",
+  "Cole" => "taco"
+}
+
+lunch_order.each { |x, y|
+    puts y
+
+  }
