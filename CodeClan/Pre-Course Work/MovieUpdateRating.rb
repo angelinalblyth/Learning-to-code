ef72d2534f3db7df17b096f54ee517e9c74c9015
Angelina <angelinalblyth@hotmail.com>
2018-04-28 10:59:28 +0100

    Update Movie title rating

---
+++ CodeClan/Pre-Course Work/MovieUpdateRating.rb
@@ -0,0 +1,38 @@
+movies = {
+  StarWars: 4.8,
+  Divergent: 4.7
+  }
+
+puts "What would you like to do? "
+
+choice = gets.chomp
+
+case choice
+when "add"
+  puts "Enter a new movie: "
+  title = gets.chomp
+  puts "Enter its rating: "
+  rating = gets.chomp
+  if movies[title.to_sym].nil?
+  movies[title.to_sym] = rating.to_i
+  puts "Added!"
+    else
+    puts "already on the system"
+  end
+when "update"
+  puts "Enter a Movie title to update: "
+  title = gets.chomp
+  if movies[title.to_sym].nil?
+    puts "Please add new title"
+    else
+    puts "Enter its new rating: "
+    rating = gets.chomp
+    movies[title.to_sym] = rating.to_i
+  end
+when "display"
+  puts "Movies!"
+when "delete"
+  puts "Deleted!"
+else
+  puts "Error!"
+end
