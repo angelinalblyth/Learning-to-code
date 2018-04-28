cf97e5175ab2fb96721a130b8bff96882605a525
Angelina <angelinalblyth@hotmail.com>
2018-04-28 10:32:53 +0100

    add movie title and rating

---
+++ CodeClan/Pre-Course Work/addtitleandrating.rb
@@ -0,0 +1,26 @@
+movies = {
+  StarWars: 4.8,
+  Divergent: 4.7
+  }
+
+puts "What would you like to do? "
+
+choice = gets.chomp
+
+case choice
+when "add"
+  puts "Enter a new movie: "
+  title = gets.chomp
+  puts "Enter its rating: "
+  rating = gets.chomp
+  movies[title] = rating
+  puts "Added!"
+when "update"
+  puts "Updated!"
+when "display"
+  puts "Movies!"
+when "delete"
+  puts "Deleted!"
+else
+  puts "Error!"
+end
