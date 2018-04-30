dd88749e3180b54efbab208994a36ab7dae5e84b
Angelina Blyth <angelinalblyth@hotmail.com>
2018-04-30 14:13:30 +0100

    added challenge

---
+++ CodeClan/Pre-Course Work/GettingMoreCreative.rb
@@ -0,0 +1,15 @@
+class Computer
+  @@users = {}
+    def initialize(username, password)
+      @username = username
+      @password = password
+      @files = {}
+      @@users[username] = password
+    end
+  
+  def create(filename)
+    time = Time.now
+    @files[filename] = time
+    puts ""
+  end
+end
