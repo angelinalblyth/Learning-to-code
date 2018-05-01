76a8176da15363f535001d755074aca702f59509
Angelina Blyth <angelinalblyth@hotmail.com>
2018-05-01 12:40:46 +0100

    added challenges

---
+++ CodeClan/Pre-Course Work/MultipleIngeritance.rb
@@ -0,0 +1,23 @@
+# Create your module here!
+module MartialArts
+  def swordman
+    puts "I'm a swordsman."
+  end
+end
+
+
+
+
+class Ninja
+
+  def initialize(clan)
+    @clan = clan
+  end
+end
+
+class Samurai
+
+  def initialize(shogun)
+    @shogun = shogun
+  end
+end
---
+++ CodeClan/Pre-Course Work/accessdenied.rb
@@ -0,0 +1,18 @@
+class Account
+  attr_reader :name, :balance
+  def initialize(name, balance = 100)
+    @name = name
+    @balance = balance
+  end
+  private
+  def pin
+    @pin = 1234
+  end
+  private
+  def pin_error
+    "Access denied: incorrect PIN."
+  end
+  
+end
+
+    
