b12146d004afc974bcb4054041fdeda4cccb2e2b
Angelina Blyth <angelinalblyth@hotmail.com>
2018-05-01 12:50:42 +0100

    added challenge

---
+++ CodeClan/Pre-Course Work/MakingAWithdrawal.rb
@@ -0,0 +1,34 @@
+class Account
+  attr_reader :name, :balance
+  def initialize(name, balance = 100)
+    @name = name
+    @balance = balance
+  end
+  
+  public
+  def display_balance(pin_number)
+    puts pin_number == pin ? "Balance: $#{balance}." : pin_error
+  end
+  
+  public
+  def withdraw(pin_number, amount)
+    if pin_number == pin
+    @balnce -= amount
+    puts "Withdrew #{amount}. New balance $#{@balance}."  
+    else
+    puts pin_error
+    end
+    
+  private
+  def pin
+    @pin = 1234
+  end
+  private
+  def pin_error
+    "Access denied: incorrect PIN."
+  end
+ 
+  
+end
+
+    
